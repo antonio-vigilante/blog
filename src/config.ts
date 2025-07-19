eb66f65cf24a1dafe85002ff9b06ed5912ece7ac
Antonio Vigilante <antoniovigilante@autistici.org>
2025-07-19 21:32:20 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export const SITE = {
   author: "Antonio Vigilante",
   profile: "https://antoniovigilante.pages.dev/",
   desc: "Blog di Antonio Vigilante.",
-  title: "🅰ttraversamenti",
+  title: "_attraversamenti",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 3,
